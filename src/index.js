b55d383010ac4386c6ad5f0575c615d723e28ca5
Yulize777 <ruslan602100@gmail.com>
2023-04-16 16:20:23 +0600

    webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.html'
+import './style.scss'
+import {sum,mult} from './module/calc'
+
+console.log(sum(2,3))
+console.log(mult(2,3))
---
+++ src/module/calc.js
@@ -0,0 +1,2 @@
+export const mult = (a,b) => 2 * b
+export const sum = (a,b) => 2 + b
